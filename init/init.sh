c3f1281722a9effa1019c487e717742d2e45abbb
Marnix Dessing <m.dessing@nijmegen.nl>
2025-08-29 15:44:53 +0200

    chore: remove npm from init script

--- init/init.sh
+++ init/init.sh
@@ -1,7 +1,5 @@
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 composer dump-autoload
 php artisan migrate --force
-npm install
-npm run build
 php artisan optimize:clear
 php artisan optimize
