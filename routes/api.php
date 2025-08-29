09042754c5e964e4688587c53fd8303a4a5cdc65
Michel Verhoeven <michel@woweb.nl>
2025-08-29 13:40:06 +0200

    added endpoint and controller for notifications api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Notifications;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::post('notifications', [Notifications::class, 'listen']);
+});
