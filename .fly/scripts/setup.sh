da2c2d003203c9f17d0d76d0720e69a695478ba5
Marnix Dessing <m.dessing@nijmegen.nl>
2026-02-06 15:34:50 +0100

    chore: start cron

--- .fly/scripts/setup.sh
+++ .fly/scripts/setup.sh
@@ -1,6 +1,9 @@
 echo "Setting up cronjob for scheduler"
 echo "* * * * * php /var/www/html/artisan schedule:run >> /var/log/artisan-scheduler.log 2>&1" | crontab -
 
+echo "Starting cron"
+cron
+
 # Service requires mtls files not env parameters. Container env parameters are used to configure the path to load each file from.
 echo "Setting up corsa MTLS"
 mkdir /cert
