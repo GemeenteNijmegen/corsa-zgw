cdef6bb2da39088f897c20cf42a6020062abf142
Marnix Dessing <m.dessing@nijmegen.nl>
2026-02-06 15:02:17 +0100

    chore: private key should be in .key cert in .crt

--- .fly/scripts/setup.sh
+++ .fly/scripts/setup.sh
@@ -4,6 +4,6 @@ echo "* * * * * php /var/www/html/artisan schedule:run >> /var/log/artisan-sched
 # Service requires mtls files not env parameters. Container env parameters are used to configure the path to load each file from.
 echo "Setting up corsa MTLS"
 mkdir /cert
-echo $CORSA_MTLS_PRIVATE_KEY > /cert/corsa-mtls.crt
-echo $CORSA_MTLS_CERTIFICATE > /cert/corsa-mtls.key
+echo $CORSA_MTLS_PRIVATE_KEY > /cert/corsa-mtls.key
+echo $CORSA_MTLS_CERTIFICATE > /cert/corsa-mtls.crt
 echo $CORSA_MTLS_CA_BUNDLE > /cert/corsa-mtls.pem
